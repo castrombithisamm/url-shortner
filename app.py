a57820c0902f43030c7acd55a87284e62515a92f
castrombithisamm <castro.mbithii@gmail.com>
2024-03-10 16:34:52 +0300

    add bootstrap

--- app.py
+++ app.py
@@ -4,58 +4,56 @@ import os.path
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.secret_key = "thenewboston"
+app.secret_key = 'h432hi5ohi3h5i5hi3o2hi'
 
-
-@app.route("/")  # http://
+@app.route('/')
 def home():
-    return render_template("home.html", codes=session.keys())
+    return render_template('home.html', codes=session.keys())
 
-
-
-@app.route("/your-url", methods=["GET", "POST"])  # http://localhost:5000/about
+@app.route('/your-url', methods=['GET','POST'])
 def your_url():
-    if request.method == "POST":
+    if request.method == 'POST':
         urls = {}
-        if os.path.exists("urls.json"):
-            with open("urls.json") as urls_file:
+
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
                 urls = json.load(urls_file)
-        if request.form["code"] in urls.keys():
-            flash("That short name has already been taken. Please select another name.")
-            return redirect(url_for("home"))
-        if "url" in request.form.keys():
-            urls[request.form["code"]] = {"url": request.form["url"]}
+
+        if request.form['code'] in urls.keys():
+            flash('That short name has already been taken. Please select another name.')
+            return redirect(url_for('home'))
+
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url':request.form['url']}
         else:
             f = request.files["file"]
             full_name = request.form["code"] + secure_filename(f.filename)
             f.save("/home/castro-mbithi/flaskapp/url-shortner/static/user_files/" + full_name)
             urls[request.form["code"]] = {"file": full_name}
 
-        with open("urls.json", "w") as url_file:
+        with open('urls.json','w') as url_file:
             json.dump(urls, url_file)
-            session[request.form["code"]] = True
-        return render_template("your_url.html", code=request.form["code"])
+            session[request.form['code']] = True
+        return render_template('your_url.html', code=request.form['code'])
     else:
-        return redirect(url_for("home"))
-    # return render_template("your_url.html", code = request.args['code'])
+        return redirect(url_for('home'))
 
-
-@app.route("/<string:code>")
+@app.route('/<string:code>')
 def redirect_to_url(code):
-    if os.path.exists("urls.json"):
-        with open("urls.json") as urls_file:
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
             urls = json.load(urls_file)
             if code in urls.keys():
-                if "url" in urls[code].keys():
-                    return redirect(urls[code]["url"])
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
                 else:
-                    return redirect(url_for("static", filename="user_files/" + urls[code]["file"]))
+                    return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
     return abort(404)
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("page_not_found.html"), 404
+    return render_template('page_not_found.html'), 404
 
-@app.route("/api")
+@app.route('/api')
 def session_api():
     return jsonify(list(session.keys()))
